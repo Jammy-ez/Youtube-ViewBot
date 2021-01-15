81e4120b596ac57b74dcd16d26f7cecd84b5939b
Jammy-ez <76751003+Jammy-ez@users.noreply.github.com>
2021-01-15 14:38:25 +0000

    Proxy update
    added proxy option

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from selenium.webdriver.common.by import By
 import time
 from os import system, name
 from colorama import Fore, Back, Style
-import pyfiglet
 def clear():
         _ = system('cls')
 print(Fore.RED + '''
@@ -38,14 +37,14 @@ print(Fore.RED + '''
 :-------------------------------------------------------------------------:
 `---._.-------------------------------------------------------------._.---'
        Type help for help
-       [1]View bot without pop up click
-       [2]View bot with pop up click
+       [1]View bot without proxies
+       [2]View bot with proxies
 ''')
 optionfirst = input("Eneter option: ")
 if optionfirst == '1':
     URL = input("Eneter youtube link: ")
     print("Views automatically set to infinite!")
-    ime.sleep(1)
+    time.sleep(1)
     WATCHTIME = input("Enter watch time: ")
     WATCHTIME = float(WATCHTIME)
     VIEWDELAY = input("Enter delay between each watch: ")
@@ -78,6 +77,8 @@ if optionfirst == '1':
     clear()
     print(Fore.RED + '''
             Youtube view bot made by jame
+
+
              ________________________________________________
             /                                                \
            |    _________________________________________     |
@@ -120,20 +121,40 @@ if optionfirst == '1':
 
 if optionfirst == '2':
     URL = input("Eneter youtube link: ")
-    time.sleep(1)
     print("Views automatically set to infinite!")
-    ime.sleep(1)
+    time.sleep(1)
     WATCHTIME = input("Enter watch time: ")
     WATCHTIME = float(WATCHTIME)
     VIEWDELAY = input("Enter delay between each watch: ")
     VIEWDELAY = float(VIEWDELAY)
+    print("Proxie should look like (11.456.448.110:8080) and should be a http/https proxie")
+    proxie1 = input("Eneter proxie 1: ")
+    proxie2 = input("Eneter proxie 2: ")
+    proxie3 = input("Eneter proxie 3: ")
+    proxie4 = input("Eneter proxie 4: ")
+    proxie5 = input("Eneter proxie 5: ")
     time.sleep(1)
     print("Settup up varibles")
-    browser1 = webdriver.Chrome()
-    browser2 = webdriver.Chrome()
-    browser3 = webdriver.Chrome()
-    browser4 = webdriver.Chrome()
-    browser5 = webdriver.Chrome()
+    #browser 1
+    chrome_options = WebDriver.ChromeOptions()
+    chrome_options.add_argument('--proxy-server=%s' % proxie1)
+    browser1 = webdriver.Chrome(chrome_options=chrome_options)
+    #browser 2
+    chrome_options2 = WebDriver.ChromeOptions()
+    chrome_options2.add_argument('--proxy-server=%s' % proxie2)
+    browser2 = webdriver.Chrome(chrome_options2=chrome_options2)
+    #browser 3
+    chrome_options3 = WebDriver.ChromeOptions()
+    chrome_options3.add_argument('--proxy-server=%s' % proxie3)
+    browser3 = webdriver.Chrome(chrome_options3=chrome_options3)
+    #browser 4
+    chrome_options4 = WebDriver.ChromeOptions()
+    chrome_options4.add_argument('--proxy-server=%s' % proxie4)
+    browser4 = webdriver.Chrome(chrome_options4=chrome_options4)
+    #browser 5
+    chrome_options5 = WebDriver.ChromeOptions()
+    chrome_options5.add_argument('--proxy-server=%s' % proxie5)
+    browser5 = webdriver.Chrome(chrome_options5=chrome_options5)
     time.sleep(1)
     print("Drivers getting link...")
     browser1.get(URL)
@@ -155,6 +176,8 @@ if optionfirst == '2':
     clear()
     print(Fore.RED + '''
             Youtube view bot made by jame
+
+
              ________________________________________________
             /                                                \
            |    _________________________________________     |
@@ -185,34 +208,12 @@ if optionfirst == '2':
 `---._.-------------------------------------------------------------._.---'
     ''')
     while True:
-       print("Please wait while all the videos load...")
-    time.sleep(10)
-    browser1.find_element_by_xpath( "//button[contains(text(), 'NO THANKS')]")\
-         .click()
-    browser1.find_element_by_xpath( "//button[contains(text(), 'I agree')]")\
-        .click()
-    browser2.find_element_by_xpath( "//button[contains(text(), 'NO THANKS')]")\
-        .click()
-    browser2.find_element_by_xpath( "//button[contains(text(), 'I agree')]")\
-         .click()
-    browser3.find_element_by_xpath( "//button[contains(text(), 'NO THANKS')]")\
-         .click()
-    browser3.find_element_by_xpath( "//button[contains(text(), 'I agree')]")\
-        .click()
-    browser4.find_element_by_xpath( "//button[contains(text(), 'NO THANKS')]")\
-        .click()
-    browser4.find_element_by_xpath( "//button[contains(text(), 'I agree')]")\
-        .click()
-    browser5.find_element_by_xpath( "//button[contains(text(), 'NO THANKS')]")\
-        .click()
-    browser5.find_element_by_xpath( "//button[contains(text(), 'I agree')]")\
-        .click()
-    time.sleep(WATCHTIME)
-    browser1.refresh()
-    browser2.refresh()
-    browser3.refresh()
-    browser4.refresh()
-    browser5.refresh()
-    print("All browsers finished")
-    time.sleep(VIEWDELAY)
-    print("View delay finished")
+     time.sleep(WATCHTIME)
+     browser1.refresh()
+     browser2.refresh()
+     browser3.refresh()
+     browser4.refresh()
+     browser5.refresh()
+     print("All browsers finished")
+     time.sleep(VIEWDELAY)
+     print("View delay finished")
